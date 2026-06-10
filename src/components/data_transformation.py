5c63204f900f49c851de96e5803be760de488ea1
nalin1212 <145797322+nalin1212@users.noreply.github.com>
2026-06-10 22:45:54 +0530

    Data_Transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -73,9 +73,9 @@ class DataTransformation:
             numerical_columns = ["writing_score", "reading_score"]
             
 
-            input_feature_train_df = train_df.drop(columns=target_column_name, axis=1)
+            input_feature_train_df = train_df.drop(target_column_name, axis=1)
             target_feature_train_df = train_df[target_column_name]
-            input_feature_test_df = test_df.drop(columns=target_column_name, axis=1)
+            input_feature_test_df = test_df.drop(target_column_name, axis=1)
             target_feature_test_df = test_df[target_column_name]
 
             input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
